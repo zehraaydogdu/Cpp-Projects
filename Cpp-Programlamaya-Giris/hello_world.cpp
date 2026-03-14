61740e6f702fe2e5bda0546e928e5aebd517b055
Zehra Aydoğdu <zehraaydogdu74@gmail.com>
2026-03-14 14:52:43 +0300

    C++ programlamaya girişe dair notlar alındı, kaynak kodlar eklendi

---
+++ Cpp-Programlamaya-Giris/hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std; //standart kütüphaneyi kullanacağımızı belirtir.
+int main(){
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+/*Eğer using namespace std; kullanılmazsa, cout ve endl yerine std::cout ve std::endl
+kullanılmalıdır.*/
